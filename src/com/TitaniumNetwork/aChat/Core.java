d2bebc90eaa1d279487b2d6c7507f096b6dfa04e
TitaniumNetwork <christyg1@hotmail.co.uk>
2016-08-23 23:45:48 +0100

    Initial commit.

---
+++ src/com/TitaniumNetwork/aChat/Core.java
@@ -0,0 +1,13 @@
+package com.TitaniumNetwork.aChat;
+
+import org.bukkit.craftbukkit.libs.jline.internal.Log;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin{
+	
+	@Override
+	public void onEnable(){
+		Log.info("[aChat] Enabled!");
+	}
+
+}
